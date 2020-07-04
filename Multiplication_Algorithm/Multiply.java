e053d59d9401b407d77d0461401358fb351ba7b2
Bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-07-03 18:56:07 -0700

    implement intro() function

--- Multiplication_Algorithm/Multiply.java
+++ Multiplication_Algorithm/Multiply.java
@@ -2,6 +2,21 @@ import java.util.*;
 
 public class Multiply {
 
+    // about this program
+    public void intro(){
+
+        System.out.println(
+            "\n--------------------------------------------------------------------------------------\n"
+            + "-                                                                                    -\n"
+            + "- This program demonstrates the Multiplication Algorithm of two values in two ways:  -\n"
+            + "-                                                                                    -\n"
+            + "- Option (1): The Partial-Products Method                                            -\n"
+            + "- Option (2): The Standard Multiplication Method                                     -\n"
+            + "-             Also known as the shortcut method of option (1).                       -\n"
+            + "--------------------------------------------------------------------------------------\n\n"
+        );
+    }
+
     public static void main(String[] args) {
 
     }
